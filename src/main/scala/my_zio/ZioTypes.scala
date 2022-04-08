dce73605f4c6a65edddc2d039e4ecff19351b49b
robert.makyla <robert.makyla@gmail.com>
2022-04-08 14:47:23 +0200

    zio - types, errorRecovery

--- src/main/scala/my_zio/ZioTypes.scala
+++ src/main/scala/my_zio/ZioTypes.scala
@@ -6,25 +6,24 @@ import zio._
 
 object ZioTypes {
 
-  type Task[+A] = ZIO[Any, Throwable, A]
+  type Task[+A] = ZIO[Any, Throwable, A] // jak funkcja Javy
 
   type UIO[+A] = ZIO[Any, Nothing, A]
 
   type RIO[-R, +A] = ZIO[R, Throwable, A]
 
-  type IO[+E, +A] = ZIO[Any, E, A]
+  type IO[+E, +A] = ZIO[Any, E, A] // wszystko może miec
 
   type URIO[-R, +A] = ZIO[R, Nothing, A]
 }
 
 object HelloWorld extends ZIOAppDefault {
 
+  import Console._
+  def run: ZIO[Has[Console], IOException, Unit] = printLine("Hello, World!")
 
-  def run: ZIO[Has[Console], IOException, Unit] =
-    Console.printLine("Hello, World!")
-
-  //  map() or as(): if we need to change success type:
-  // (Console.printLine("hello") as 0) // or map(_ => 0)
+  //  map(_ => 1) or as(): if we need to change success type:
+  // (printLine("hello") as 0) // or map(_ => 0)
 }
 
 object PrintSequence extends ZIOAppDefault {
@@ -39,16 +38,16 @@ object ErrorRecovery extends ZIOAppDefault {
 
   import Console._
 
-  def failablePrint(s: String): ZIO[Has[Console], IOException, Unit] = printLine(s)
-
-  def unfailablePrint(s: String): ZIO[Has[Console], Nothing, Unit] = printLine(s) orElse ZIO.succeed(()) // orElse() gets rid of error
+  def failablePrint(s: String):            ZIO[Has[Console], IOException, Unit] = printLine(s)
+  def unfailablePrint(s: String):          ZIO[Has[Console], Nothing, Unit] = printLine(s) orElse ZIO.succeed(()) // orElse() - runs the second effect if the first one failed.
+  def unfailablePrintWithCatch(s: String): ZIO[Has[Console], Nothing, Unit] = printLine(s).catchAllCause(cause => unfailablePrint(cause.prettyPrint)) // catchAll[Cause] catches error and returns second effect
 
   val failWithString: IO[String, Nothing] = ZIO.fail[String]("BOOM")
 
   def run: ZIO[Has[Console], IOException, Unit] = {
 
     val res: ZIO[Has[Console], String, Unit] =
-      unfailablePrint("I am about to fail") *> // *> or zipRight()
+      unfailablePrintWithCatch("I am about to fail") *> // *> is the same as zipRight()
         failWithString *>
         unfailablePrint("I will never be printed")
 
@@ -59,6 +58,4 @@ object ErrorRecovery extends ZIOAppDefault {
     res.mapError((msg: String) => new IOException(msg)) // mapError: just change failure
   }
 
-//teraz minuta 16
-  //https://www.youtube.com/watch?v=TWdC7DhvD8M
 }
